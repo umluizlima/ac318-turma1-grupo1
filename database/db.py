d2f28b385f8feab79922f2896af66091a50a125f
umluizlima <umluizlima@gmail.com>
2018-04-03 17:47:56 -0300

    Refactor do db init

--- database/db.py
+++ database/db.py
@@ -1,3 +1,13 @@
+import os
+import sqlite3
 import dataset
 
 db = dataset.connect('sqlite:///database/contacts.db', ensure_schema=False)
+
+if __name__ == "__main__":
+    if not os.path.lexists(os.path.join('database', 'contacts.db')):
+        db = sqlite3.connect(os.path.join('database', 'contacts.db'))
+        with open(os.path.join('database', 'schema.sql'), mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        db.close()
--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-import os
-import sqlite3
 from app import app
-
-if not os.path.lexists(os.path.join('database', 'contacts.db')):
-    db = sqlite3.connect(os.path.join('database', 'contacts.db'))
-    with open(os.path.join('database', 'schema.sql'), mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
 
 app.run(host='0.0.0.0',
     port=5555,
